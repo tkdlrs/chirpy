9b921b3f1472d1e24fe327b3e7fb1b8dbce587a2
tkdlrs <tkd.lsanchez@gmail.com>
2025-06-23 18:33:47 -0600

    Updated code after looking at solution files.

--- main.go
+++ main.go
@@ -1,48 +1,20 @@
-package chirpy
+package main
 
 import (
-	"context"
-	"crypto/tls"
 	"log"
-	"net"
 	"net/http"
-	"time"
 )
 
-type Server struct {
-	Addr string
-
-	Handler Handler // handler to invoke, http.DefaultServeMux if nil
-
-	DisableGeneralOptionsHandler bool
-
-	TLSConfig *tls.Config
-
-	ReadTimeout time.Duration
-
-	ReadHeaderTimeout time.Duration
-
-	WriteTimeout time.Duration
-
-	IdleTimeout time.Duration
-
-	MaxHeaderBytes int
-
-	TLSNextProto map[string]func(*Server, *tls.Conn, Handler)
-
-	ConnState func(net.Conn, ConnState)
-
-	ErrorLog *log.Logger
-
-	BaseContext func(net.Listener) context.Context
-
-	ConnContext func(ctx context.Context, c net.Conn) context.Context
-
-	HTTP2 *HTTP2Config
-
-	Protocols *Protocols
-}
-
 func main() {
-	mux := http.NewServeMux
+	const port = "8080"
+	//
+	mux := http.NewServeMux()
+	//
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+	//
+	log.Printf("Servering on port: %s\n", port)
+	log.Fatal(server.ListenAndServe())
 }
