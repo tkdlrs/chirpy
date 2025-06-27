8b7c488054cea662badbafc18622ad91bb87faf6
tkdlrs <tkd.lsanchez@gmail.com>
2025-06-26 19:51:12 -0600

    Reviews the solution files for Chapter 1: Server; Lesson 11: Custom Handlers of the 'Learn HTTP Servers in Go course' and makes some updates to better align with the course and solidify my understanding.

--- main.go
+++ main.go
@@ -1,22 +1,17 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
-	const filepathRoot = ""
+	const filepathRoot = "."
 	const port = "8080"
 	//
 	mux := http.NewServeMux()
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		io.WriteString(w, "OK")
-	})
 	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 	//
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -25,4 +20,10 @@ func main() {
 	//
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
