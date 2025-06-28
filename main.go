d160d1c8ba34080c93d9210fba788085c4e13089
tkdlrs <tkd.lsanchez@gmail.com>
2025-06-28 10:54:23 -0600

    My attempt at 'Ch2: Routing, L1: Stateful Handlers' in the 'Learn HTTP Servers in Go' course on Boot.dev. I was not able to figure it out on my own. Middleware is still a mystery to me. I will understand middleware completely and soon, but I haven't got it quite yet. Working on it.

--- main.go
+++ main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	cfg.fileserverHits.Add(1)
+	return next
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 	//
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	apiCfg := apiConfig{fileserverHits: atomic.Int32{}}
+	//
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerServerHits)
+	// mux.HandleFunc("reset", apiCfg. )
 	//
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -27,3 +42,24 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
+
+func (cfg *apiConfig) handlerServerHits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	// io.WriteString(w, "Hits: ")
+	fmt.Fprintf(w, "Hits: %v", cfg.fileserverHits.Load())
+}
+
+// func (cfg *apiConfig) getServerHits() int32 {
+// 	return int32(cfg.fileserverHits.Load())
+// }
+
+// func handlerResetServerHits(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+// 	w.WriteHeader(http.StatusOK)
+// 	// resetSeverHit()
+// 	fmt.Fprintf(w, "Server counts reset")
+// }
+
+// func resetSeverHits(cfg apiConfig) {
+// }
