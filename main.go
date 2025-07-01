c7f2124de4fa56c8bb75ca45e7fb5577f045a3bf
tkdlrs <tkd.lsanchez@gmail.com>
2025-07-01 17:10:30 -0600

    Reviews solution files for 'Ch4: JSON | L2: JSON' and aligns my code with the course.

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 	"sync/atomic"
@@ -23,7 +22,7 @@ func main() {
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	//
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
-	mux.HandleFunc("POST /api/validate_chirp", handleChirpValidation)
+	mux.HandleFunc("POST /api/validate_chirp", handleChirpValidate)
 	//
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
@@ -36,55 +35,4 @@ func main() {
 	//
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
-}
-
-func handleChirpValidation(w http.ResponseWriter, r *http.Request) {
-	// Decode the JSON of the chirp
-	type chirp struct {
-		Body string `json:"body"`
-	}
-	decoder := json.NewDecoder(r.Body)
-	params := chirp{}
-	err := decoder.Decode(&params)
-	if err != nil {
-		log.Printf("Error decoding chirp body: %s", err)
-		w.WriteHeader(500)
-		return
-	}
-	//
-	type returnErrorVal struct {
-		Error string `json:"error"`
-	}
-	type returnValidVal struct {
-		Valid bool `json:"valid"`
-	}
-	// check at it's valid and encode a response
-	if len(params.Body) > 140 {
-		respBody := returnErrorVal{
-			Error: "Chirp is too Long",
-		}
-		dat, err := json.Marshal(respBody)
-		if err != nil {
-			respBody.Error = "Something went wrong"
-			w.WriteHeader(500)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(400)
-		w.Write(dat)
-		return
-	}
-	//
-	respBody := returnValidVal{
-		Valid: true,
-	}
-	dat, err := json.Marshal(respBody)
-	if err != nil {
-		respBody.Valid = false
-		w.WriteHeader(500)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-	w.Write(dat)
 }
