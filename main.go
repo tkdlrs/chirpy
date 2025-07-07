46d85c43defa00a1e9fb3379ed3b905def03d470
tkdlrs <tkd.lsanchez@gmail.com>
2025-07-07 13:31:50 -0600

    Finishes 'Chapter 5: Storage | Lesson 5: Create User' without looking at the solution files.

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"sync/atomic"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/tkdlrs/chirpy/internal/database"
@@ -16,6 +19,7 @@ import (
 type apiConfig struct {
 	fileserverHits atomic.Int32
 	dbQueries      *database.Queries
+	platform       string
 }
 
 func main() {
@@ -33,6 +37,7 @@ func main() {
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
 		dbQueries:      dbQueries,
+		platform:       os.Getenv("PLATFORM"),
 	}
 	//
 	mux := http.NewServeMux()
@@ -40,6 +45,8 @@ func main() {
 	//
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 	mux.HandleFunc("POST /api/validate_chirp", handleChirpValidate)
+	//
+	mux.HandleFunc("POST /api/users", apiCfg.handleCreateUser)
 	//
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
@@ -52,4 +59,41 @@ func main() {
 	//
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+}
+
+func (cfg *apiConfig) handleCreateUser(w http.ResponseWriter, r *http.Request) {
+	// Set up stuff to get info out of request
+	type parameters struct {
+		Email string `json:"email"`
+	}
+	type returnVals struct {
+		User
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
+	// Use the info from appove to CREATE a user in the database
+	user, err := cfg.dbQueries.CreateUser(r.Context(), params.Email)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not create a user", nil)
+	}
+	// Happy path, return newly created user
+	newUser := User{
+		ID:        user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email:     user.Email,
+	}
+	respondWithJSON(w, http.StatusCreated, returnVals{newUser})
 }
--- reset.go
+++ reset.go
@@ -1,9 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	// Check for correct enivornment
+	if cfg.platform != "dev" {
+		respondWithError(w, http.StatusForbidden, "Incorrect platform", nil)
+	}
+	// Delete all users from the database.
+	err := cfg.dbQueries.DeleteUsers(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "unable to delete all users", err)
+		return
+	}
+	//
 	cfg.fileserverHits.Store(0)
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits reset to 0\n"))
+	w.Write([]byte("Hits reset to 0 and all users removed from Postgres Database.\n"))
 }
